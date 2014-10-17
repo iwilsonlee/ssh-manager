dfe83c57d4f2360c9c545846be7bbbd37cedd5ac
Wilson <wensonlee@gmail.com>
2014-10-17 18:57:12 +0800

    just for osx

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
         buildDir: './build', // Where the build version of my node-webkit app is saved
         credits: './public/Credits.html',
         macIcns: './icon.icns', // Path to the Mac icon file
-        platforms: ['osx', 'win'] // These are the platforms that we want to build
+        platforms: ['osx'] // These are the platforms that we want to build
       },
       src: './public/**/*' // Your node-webkit app
     },
